7405ce0f05b79aa56dd788054bc5af009ee2fb70
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-03-24 22:29:04 -0300

    Conectando-MySQL

--- Arquivos/Sistema/Sistema_Financas.dpr
+++ Arquivos/Sistema/Sistema_Financas.dpr
@@ -2,13 +2,15 @@ program Sistema_Financas;
 
 uses
   Vcl.Forms,
-  Uprincipal in 'Uprincipal.pas' {Form1};
+  Uprincipal in 'Uprincipal.pas' {frmprincipal},
+  ModConexao in 'ModConexao.pas' {DataModule1: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(Tfrmprincipal, frmprincipal);
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
--- Arquivos/Sistema/Uprincipal.pas
+++ Arquivos/Sistema/Uprincipal.pas
@@ -4,10 +4,30 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
+  Vcl.StdCtrls, Vcl.ComCtrls;
 
 type
-  TForm1 = class(TForm)
+  Tfrmprincipal = class(TForm)
+    Label1: TLabel;
+    imgusuarios: TImage;
+    imgreceber: TImage;
+    imgpagar: TImage;
+    imgcaixa: TImage;
+    imgconreceber: TImage;
+    imgconpagar: TImage;
+    Label3: TLabel;
+    Label4: TLabel;
+    imgrelpagar: TImage;
+    imgnavegador: TImage;
+    imgconfig: TImage;
+    imgrelreceber: TImage;
+    imgrelcaixa: TImage;
+    BalloonHint1: TBalloonHint;
+    StatusBar1: TStatusBar;
+    Timer1: TTimer;
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
     { Private declarations }
   public
@@ -15,10 +35,26 @@ type
   end;
 
 var
-  Form1: TForm1;
+  frmprincipal: Tfrmprincipal;
 
 implementation
 
 {$R *.dfm}
+
+procedure Tfrmprincipal.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+if Application.MessageBox('Desja Realmente Sair?', 'Informação', MB_YESNO+MB_ICONQUESTION) = mrYES then
+
+Application.Terminate
+
+else
+Abort;
+
+end;
+
+procedure Tfrmprincipal.Timer1Timer(Sender: TObject);
+begin
+  StatusBar1.Panels.Items[0].Text := DateTimeToStr(now);
+end;
 
 end.
