e21ffedc6ace37371bbbfd849ddd245222adb4c2
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-03-25 17:21:50 -0300

    Configurando-Sistema-Financeiro

--- Arquivos/Sistema/Sistema_Financas.dpr
+++ Arquivos/Sistema/Sistema_Financas.dpr
@@ -2,8 +2,7 @@ program Sistema_Financas;
 
 uses
   Vcl.Forms,
-  Uprincipal in 'Uprincipal.pas' {frmprincipal},
-  ModConexao in 'ModConexao.pas' {DataModule1: TDataModule};
+  Uprincipal in 'Uprincipal.pas' {frmprincipal};
 
 {$R *.res}
 
@@ -11,6 +10,5 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(Tfrmprincipal, frmprincipal);
-  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
--- Arquivos/Sistema/Uprincipal.pas
+++ Arquivos/Sistema/Uprincipal.pas
@@ -5,29 +5,24 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
-  Vcl.StdCtrls, Vcl.ComCtrls;
+  Vcl.StdCtrls;
 
 type
   Tfrmprincipal = class(TForm)
     Label1: TLabel;
-    imgusuarios: TImage;
-    imgreceber: TImage;
-    imgpagar: TImage;
-    imgcaixa: TImage;
-    imgconreceber: TImage;
-    imgconpagar: TImage;
+    Label2: TLabel;
     Label3: TLabel;
-    Label4: TLabel;
-    imgrelpagar: TImage;
-    imgnavegador: TImage;
-    imgconfig: TImage;
-    imgrelreceber: TImage;
-    imgrelcaixa: TImage;
-    BalloonHint1: TBalloonHint;
-    StatusBar1: TStatusBar;
-    Timer1: TTimer;
-    procedure Timer1Timer(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    imgusuarios: TImage;
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Image5: TImage;
+    Image6: TImage;
+    Image7: TImage;
+    Image8: TImage;
+    Image9: TImage;
+    Image10: TImage;
   private
     { Private declarations }
   public
@@ -40,21 +35,5 @@ var
 implementation
 
 {$R *.dfm}
-
-procedure Tfrmprincipal.FormClose(Sender: TObject; var Action: TCloseAction);
-begin
-if Application.MessageBox('Desja Realmente Sair?', 'Informação', MB_YESNO+MB_ICONQUESTION) = mrYES then
-
-Application.Terminate
-
-else
-Abort;
-
-end;
-
-procedure Tfrmprincipal.Timer1Timer(Sender: TObject);
-begin
-  StatusBar1.Panels.Items[0].Text := DateTimeToStr(now);
-end;
 
 end.
